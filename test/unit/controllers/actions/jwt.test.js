4b1f583913e12acbddb1ca9d6bedc1379f271b9b
Zilkod <83935849+Zilkod@users.noreply.github.com>
2024-08-14 23:25:05 +0600

    .

---
+++ test/unit/controllers/actions/jwt.test.js
@@ -0,0 +1,136 @@
+'use strict';
+
+var proxyquire = require('proxyquire');
+var should = require('should');
+var mocha = require('mocha');
+var config = require('../../../fixtures/waterlock.config').waterlock;
+var createJwt = require('../../../../lib/utils').createJwt;
+
+describe('actions', function(){
+  describe('jwt', function(){
+    var jwt =  require('../../../../lib/controllers/actions/jwt');
+    it('should return forbidden if session not authenticated', function(done){
+      var req = {
+        session:{
+          authenticated: false
+        }
+      };
+      var res = {
+        forbidden:function(str){
+          str.should.be.type('string');
+          done();
+        }
+      };
+      jwt(req, res);
+    });
+    it('should create a jwt', function(done){
+      var req = {
+        session:{
+          authenticated: true,
+          user:{
+            id: 1
+          }
+        }
+      };
+      var res = {
+        json:function(obj){
+          obj.should.be.type('object');
+          obj.should.have.property('token');
+          obj.should.have.property('expires');
+          obj.token.should.be.type('string');
+          done();
+        }
+      };
+      global.Jwt = {
+        create: function(){
+          return {
+            exec: function(cb){
+              cb(null);
+            }
+          };
+        }
+      };
+      global.waterlock = {
+        config: config,
+        _utils: {
+          createJwt: createJwt
+        }
+      };
+
+      jwt.apply(this, [req, res]);
+    });
+    it('should create a jwt and return User', function(done){
+      var localConfig = config;
+      localConfig.jsonWebTokens.includeUserInJwtResponse = true;
+      var req = {
+        session:{
+          authenticated: true,
+          user:{
+            id: 1
+          }
+        }
+      };
+      var res = {
+        json:function(obj){
+          obj.should.be.type('object');
+          obj.should.have.property('token');
+          obj.should.have.property('expires');
+          obj.token.should.be.type('string');
+          obj.user.should.be.type('object');
+          obj.user.should.have.property('id');
+          done();
+        }
+      };
+      global.Jwt = {
+        create: function(){
+          return {
+            exec: function(cb){
+              cb(null);
+            }
+          };
+        }
+      };
+      global.waterlock = {
+        config: localConfig,
+        _utils: {
+          createJwt: createJwt
+        }
+      };
+
+      jwt.apply(this, [req, res]);
+    });
+    it('should return a serverError if a jwt cannot be created', function(done){
+      var req = {
+        session:{
+          authenticated: true,
+          user:{
+            id: 1
+          }
+        }
+      };
+      var res = {
+        serverError:function(str){
+          str.should.be.type('string');
+          done();
+        }
+      };
+      global.Jwt = {
+        create: function(){
+          return {
+            exec: function(cb){
+              cb('agh');
+            }
+          };
+        }
+      };
+      global.waterlock = {
+        config: config,
+        _utils: {
+          createJwt: createJwt
+        }
+      };
+
+      jwt.apply(this, [req, res]);
+    });
+  });
+});
---
+++ test/unit/utils.test.js
@@ -0,0 +1,136 @@
+'use strict';
+
+var proxyquire = require('proxyquire');
+var should = require('should');
+var mocha = require('mocha');
+var utils = require('../../lib/utils');
+var config = require('../fixtures/waterlock.config').waterlock;
+
+describe('utils', function(){
+  describe('#allParams()', function(){
+    it('should collect all request parameter including headers', function(done){
+      var req = {
+        params: {
+          all: function(){
+            return {
+              foo: 'bar'
+            };
+          }
+        },
+        headers: {
+          faz: 'baz'
+        },
+        query: {
+          shake: 'bake'
+        }
+      };
+
+      var results = utils.allParams(req);
+
+      results.should.have.property('foo');
+      results.should.have.property('faz');
+      results.should.have.property('shake');
+      done();
+    });
+  });
+
+  describe('#countTopLevel()', function(){
+    it('should count only the top level of an object', function(done){
+      var obj = {
+        foo: 'bar',
+        faz: {
+          shake: 'bake'
+        }
+      };
+      var count = utils.countTopLevel(obj);
+      count.should.eql(2);
+      done();
+    });
+
+    it('should return -1 if given param is not an object', function(done){
+      var shake = 'bake';
+      var count = utils.countTopLevel(shake);
+      count.should.eql(-1);
+      done();
+    });
+  });
+
+  describe('#accessObjectLikeArray()', function(){
+    it('should return 2nd parameter if it\'s not an object', function(done){
+      var shake = 'bake';
+      var result = utils.accessObjectLikeArray(2, shake);
+      result.should.eql('bake');
+      done();
+    });
+    it('should return value at position if given array', function(done){
+      var shake = ['bake'];
+      var result = utils.accessObjectLikeArray(0, shake);
+      result.should.eql('bake');
+      done();
+    });
+    it('should return 2nd object in object', function(done){
+      var shake = {shake:'bake',magic:'man'};
+      var result = utils.accessObjectLikeArray(1, shake);
+      result.should.eql('man');
+      done();
+    });
+  });
+
+  describe('#createJwt()', function(){
+    it('should create Jwt token including expires and token property', function(done){
+      var req = {
+        session:{
+          authenticated: true,
+          user:{
+            id: 1
+          }
+        }
+      };
+      var res = {
+        json:function(obj){
+          obj.should.be.type('object');
+          obj.should.have.property('token');
+          obj.should.have.property('expires');
+          obj.token.should.be.type('string');
+          done();
+        }
+      };
+      global.waterlock = { config: config };
+      var result = utils.createJwt(req, res);
+      result.should.have.property('expires');
+      result.should.have.property('token');
+      done();
+    });
+  });
+
+  describe('#getAccessToken()', function(){
+    it('should return token from params', function(done){
+      var req = {
+        params: {
+          all: function(){
+            return {
+                access_token: 'bar'
+            };
+          }
+        }
+      };
+
+      var results = utils.getAccessToken(req);
+
+      results.should.be.exactly('bar');
+      done();
+    });
+    it('should return token from headers(Authorization)', function(done){
+      var req = {
+          headers:{
+              authorization: 'Bearer baz'
+          }
+      };
+
+      var results = utils.getAccessToken(req);
+
+      results.should.be.exactly('baz');
+      done();
+    });
+  });
+});
